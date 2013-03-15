a96d7757081a4834453da272dae0bc6b084c715d
connor McCrone <your.email@example.com>
2013-03-15 00:38:47 -0400

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -18,7 +18,6 @@ describe "User pages" do
     it { should have_selector('h1',    text: user.name) }
     it { should have_selector('title', text: user.name) }
   end
-end
 
 describe "signup" do
 
@@ -43,5 +42,11 @@ describe "signup" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+    describe "after saving the user" do
+        before { click_button submit }
+        it { should have_link('Sign out') }
+      end
     end
-  end
+  end 
+end       
